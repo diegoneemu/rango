f520569142f34f350493b3a8aba985cbc5e2283a
Diego Augusto <diego.augusto@neemu.com>
2021-07-31 13:38:47 -0300

    feat(<Home />): ensure that render a restaurant search field

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -4,6 +4,7 @@ export const Home: FunctionComponent = () => {
   return (
     <div className="home_page">
       <h1>Bem-vindo ao Lista Rango</h1>
+      <input type="text" placeholder="Buscar estabelecimento" />
     </div>
   );
 }
