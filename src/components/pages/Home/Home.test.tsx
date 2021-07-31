6fb80e4817009a41b7c902d5626c41e6cf42e81c
Diego Augusto <diego.augusto@neemu.com>
2021-07-31 14:55:58 -0300

    test(<Home />): ensure that render a restaurant cards

--- src/components/pages/Home/Home.test.tsx
+++ src/components/pages/Home/Home.test.tsx
@@ -15,4 +15,26 @@ describe("<Home />", () => {
     const restaurantSearchField = screen.queryByPlaceholderText("Buscar estabelecimento");
     expect(restaurantSearchField).toBeInTheDocument();
   });
+
+  test("Should be render a restaurant cards", () => {
+    const {debug } = render(<Home />);
+    const restaurantCards = screen.queryAllByRole("button",{name: /Nome do Restaurante/i});
+    expect(restaurantCards.length).toEqual(12);
+    debug();
+    restaurantCards.forEach((restaurantCard: HTMLElement, index: number)=>{
+      expect(restaurantCard).toHaveAttribute("tabindex");
+      expect(restaurantCard.getAttribute("tabindex")).toEqual(index.toString());
+      expect(restaurantCard).toHaveTextContent(/Nome do Restaurante/i);
+      expect(restaurantCard).toHaveTextContent(/Endereço do restaurante/i);
+      expect(restaurantCard).toHaveAccessibleName(/Nome do Restaurante/i);
+      expect(restaurantCard).toHaveTextContent(/(Aberto agora)|(Fechado)/i);
+
+      const restaurantLogo = restaurantCard.querySelector("img") as HTMLImageElement;
+      const src = restaurantLogo.getAttribute("src");
+      const alt = restaurantLogo.getAttribute("alt");
+
+      expect(src).toEqual("img/nome_do_restaurante.png");
+      expect(alt).toEqual("Logotipo do Nome do Restaurante");
+    })
+  })
 });
