01264157a6720b3d257b28b4ccbe0065471e5082
Diego Augusto <diego.augusto@neemu.com>
2021-07-31 13:34:00 -0300

    test(<Home />): ensure that render a header title

--- src/components/pages/Home/Home.test.tsx
+++ src/components/pages/Home/Home.test.tsx
@@ -1,3 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { Home } from ".";
+
 describe("<Home />", () => {
-  test.todo('Should be render a header title');
-})
+  test("Should be render a header title", () => {
+    render(<Home />);
+    const headerTitle = screen.queryByRole("heading", {
+      name: /Bem-vindo ao Lista Rango/i,
+    });
+    expect(headerTitle).toBeInTheDocument();
+  });
+});
