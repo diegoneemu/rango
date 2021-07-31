cb9234221ce26de1ada72d9d4cd0ad5b3972671a
Diego Augusto <diego.augusto@neemu.com>
2021-07-31 14:56:14 -0300

    feat(<Home />): ensure that render a restaurant cards

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -5,6 +5,16 @@ export const Home: FunctionComponent = () => {
     <div className="home_page">
       <h1>Bem-vindo ao Lista Rango</h1>
       <input type="text" placeholder="Buscar estabelecimento" />
+      {Array.from(
+        { length: 12 }, 
+        (_: unknown, index: number) => 
+          (<article key={index} aria-label="Nome do Restaurante" tabIndex={index} role="button">
+            <h1>Nome do Restaurante</h1>
+            <h2>Endereço do restaurante</h2>
+            <img alt="Logotipo do Nome do Restaurante" src="img/nome_do_restaurante.png" />
+            <span>Fechado</span>
+          </article>)
+      )}
     </div>
   );
 }
