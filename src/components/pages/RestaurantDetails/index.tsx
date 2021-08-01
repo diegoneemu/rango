2ef15d4b297f5edecf06df674b1fccbe6b6d5853
Diego Augusto <diego.augusto@neemu.com>
2021-08-01 17:59:03 -0300

    feat(<RestauranteDetails />): ensure that render a restaurant address

--- src/components/pages/RestaurantDetails/index.tsx
+++ src/components/pages/RestaurantDetails/index.tsx
@@ -3,5 +3,7 @@ import { FunctionComponent } from "react";
 export const RestaurantDetails: FunctionComponent = () => {
   return (<div>
     <img alt="Logotipo do Nome do Restaurante" src="img/0_nome_do_restaurante.png" />
-    <h1>Nome do Restaurante</h1></div>);
+    <h1>Nome do Restaurante</h1>
+    <p aria-label="Endereço do Restaurante">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>);
 };
