fb930e3e7b40e593c1cffa9fa27bfe8231563868
Diego Augusto <diego.augusto@neemu.com>
2021-08-01 18:04:08 -0300

    feat(<RestauranteDetails />): ensure that render a restaurant opening hours

--- src/components/pages/RestaurantDetails/index.tsx
+++ src/components/pages/RestaurantDetails/index.tsx
@@ -5,5 +5,10 @@ export const RestaurantDetails: FunctionComponent = () => {
     <img alt="Logotipo do Nome do Restaurante" src="img/0_nome_do_restaurante.png" />
     <h1>Nome do Restaurante</h1>
     <p aria-label="Endereço do Restaurante">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <article aria-label="Horário de Funcionamento">
+      <p>Segunda à Sexta: 11:30 às 15:00</p>
+      <p>Sábados: 11:30 às 22:00</p>
+      <p>Domingos e Feriados: 11:30 às 15:00</p>
+    </article>
   </div>);
 };
