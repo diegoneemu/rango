50d21cd84c96e2b6debfa6f3afb7c1fbd30dc320
Diego Augusto <diego.augusto@neemu.com>
2021-08-01 17:51:45 -0300

    feat(<RestaurantDetails />): ensure that render a restaurant logo

--- src/components/pages/RestaurantDetails/index.tsx
+++ src/components/pages/RestaurantDetails/index.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from "react";
 
 export const RestaurantDetails: FunctionComponent = () => {
-  return (<div><h1>Nome do Restaurante</h1></div>);
+  return (<div>
+    <img alt="Logotipo do Nome do Restaurante" src="img/0_nome_do_restaurante.png" />
+    <h1>Nome do Restaurante</h1></div>);
 };
